5b6146ef10baeae94faa7424c40178518a40b08b
Samuel Viana <contato.santanasamuel@gmail.com>
2024-01-21 21:40:02 -0300

    featListas: Adicionando limpeza de listas, procura por elementos em posicoes especificas e insercao em posicoes especificadas

--- src/main/java/ListasEncadeadas/ListaEncadeada.java
+++ src/main/java/ListasEncadeadas/ListaEncadeada.java
@@ -5,6 +5,8 @@ public class ListaEncadeada<T>{
     private Celula<T> ultimo;
     private int tamanho;
 
+    private final int NAO_ENCONTRADO = -1;
+
     public void adiciona(T elemento){
         Celula<T> celula = new Celula<T>(elemento);
         if(getTamanho() == 0){
@@ -47,5 +49,83 @@ public class ListaEncadeada<T>{
         }while (atual.getProximo() != null);
 
         System.out.println(atual.getElemento());
+    }
+
+    public void clearList(){
+        Celula<T> atual = this.inicio;
+        while(atual != null){
+            Celula<T> proximo = atual.getProximo();
+            atual.setElemento(null);
+            atual.setProximo(null);
+            atual = proximo;
+        }
+
+        this.inicio = null;
+        this.ultimo = null;
+        this.tamanho = 0;
+    }
+
+    private Celula<T> buscaNo(int posicao){
+
+        if(!(posicao >= 0 && posicao <= this.tamanho)){
+            throw new IllegalArgumentException("Posição não existe");
+        }
+
+        Celula<T> atual = this.inicio;
+        for(int i =0; i < posicao; i++){
+            atual = atual.getProximo();
+        }
+
+        return atual;
+    }
+
+    public T buscaPorPosicao(int posicao){
+        return this.buscaNo(posicao).getElemento();
+    }
+
+    public int buscaPorElemento(T elemento){
+        Celula<T> atual = this.inicio;
+        int posicao = 0;
+        while(atual != null){
+            if(atual.getElemento().equals(elemento)){
+                return posicao;
+            }
+            atual = atual.getProximo();
+            posicao++;
+        }
+        return NAO_ENCONTRADO;
+    }
+
+    public void adicionaNoInicio(T elemento){
+        if(this.tamanho == 0){
+            Celula<T> novaCelula = new Celula<>(elemento);
+            this.inicio = novaCelula;
+            this.ultimo = novaCelula;
+        } else{
+            Celula<T> novaCelula = new Celula<>(elemento, inicio);
+            this.inicio = novaCelula;
+        }
+
+        tamanho++;
+
+    }
+
+    public void adiciona(int posicao, T elemento){
+        if(posicao == 0){
+            this.adicionaNoInicio(elemento);
+        }
+        else if(posicao == this.tamanho){
+            this.adiciona(elemento);
+        }
+        else if(posicao < 0 || posicao > this.tamanho){
+            throw new IllegalArgumentException("Posição inválida");
+        } else{
+            Celula<T> noAnterior = this.buscaNo(posicao);
+            Celula<T> proximoNo = noAnterior.getProximo();
+            Celula<T> novaCelula = new Celula<>(elemento, proximoNo);
+            noAnterior.setProximo(novaCelula);
+            this.tamanho++;
+
+        }
     }
 }
--- src/main/java/ListasEncadeadas/teste/ListaEncadeadaTeste.java
+++ src/main/java/ListasEncadeadas/teste/ListaEncadeadaTeste.java
@@ -4,6 +4,31 @@ import ListasEncadeadas.ListaEncadeada;
 
 public class ListaEncadeadaTeste {
     public static void main(String[] args) {
+        test_adicionaNoInicio();
+        test_adicionaPorPosicao();
+    }
+
+    public static void test_adicionaNoInicio(){
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        lista.adicionaNoInicio(3);
+        lista.adicionaNoInicio(2);
+        lista.adicionaNoInicio(1); //1, 2, 3
+        System.out.println(lista);
+    }
+
+    public static void test_adicionaPorPosicao(){
+        ListaEncadeada<Integer> lista = new ListaEncadeada<>();
+        lista.adiciona(1);
+        lista.adiciona( 2);
+        lista.adiciona( 4);// 1,2,4
+        lista.adiciona(0, 0);// 0, 1, 2, 4
+        lista.adiciona(4, 5); // 0, 1 2 4 5
+        lista.adiciona(2  , 3); // 0, 1 2 3 4 5
+
+        System.out.println(lista);
+    }
+
+    public static void testesIniciais(){
         ListaEncadeada lista = new ListaEncadeada<Integer>();
 
         lista.adiciona(1);
@@ -19,6 +44,22 @@ public class ListaEncadeadaTeste {
 
         System.out.println(lista);
 
+        //busca por elemento
+        System.out.println("Busca por elementos");
+        System.out.println(lista.buscaPorElemento(1));
+        System.out.println(lista.buscaPorElemento(2));
+        System.out.println(lista.buscaPorElemento(3));
+        System.out.println(lista.buscaPorElemento(0));
 
+        //busca por posicao
+        System.out.println("Busca por posicao");
+        System.out.println(lista.buscaPorPosicao(0));
+        System.out.println(lista.buscaPorPosicao(1));
+        System.out.println(lista.buscaPorPosicao(2));
+        System.out.println(lista.buscaPorPosicao(-1));
+
+
+        lista.clearList();
+        System.out.println(lista);
     }
 }
